0172f9d12e932a9353aff101a0ee7925b1f60bef
rungene <rungenesamuel@gmail.com>
2018-07-31 20:20:39 +0300

    Initial commit

---
+++ app/src/main/java/com/rungenes/firebasefirestore/MainActivity.java
@@ -0,0 +1,66 @@
+package com.rungenes.firebasefirestore;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_NUMBER = "number";
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private EditText editTextNumber;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        editTextNumber = findViewById(R.id.edit_text_number);
+    }
+
+    public void saveNote(View v) {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        String number = editTextNumber.getText().toString();
+
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_TITLE, title);
+        note.put(KEY_DESCRIPTION, description);
+        note.put(KEY_NUMBER,number);
+
+        db.collection("Notebook").document("My First Note").set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+}
