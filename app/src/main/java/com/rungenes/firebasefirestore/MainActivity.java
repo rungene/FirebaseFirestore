7e2e9913c5ca0c158859343db913097d104e9bb6
rungene <rungenesamuel@gmail.com>
2018-08-01 17:38:45 +0300

    Firestore - DELETE DATA

--- app/src/main/java/com/rungenes/firebasefirestore/MainActivity.java
+++ app/src/main/java/com/rungenes/firebasefirestore/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
@@ -71,9 +72,8 @@ public class MainActivity extends AppCompatActivity {
 
                     textViewData.setText("Title: "+title+"\n"+ "Description: "+description+"\n"+"Number "+number);
 
-
-
-
+                }else {
+                    textViewData.setText("");
                 }
 
             }
@@ -122,6 +122,23 @@ public class MainActivity extends AppCompatActivity {
       //  noteRef.set(note, SetOptions.merge());
         noteRef.update(KEY_DESCRIPTION,description);
     }
+
+    public void deleteDescription(View v){
+        String description = editTextDescription.getText().toString();
+
+      /*  Map<String,Object> note = new HashMap<>();
+        note.put(KEY_DESCRIPTION, FieldValue.delete());
+
+        noteRef.update(note);*/
+
+        noteRef.update(KEY_DESCRIPTION,FieldValue.delete());
+    }
+
+    public void deleteNote(View view){
+
+        noteRef.delete();
+    }
+
 
     public void loadNote(View v){
 
